149e3be0cd783a1e05677bc81bf2add1767ce120
Vadym-Teslytskyy <t.vadyk@gmail.com>
2018-06-06 19:19:47 +0300

    change data source creation

--- api/src/main/java/ua/dropwizzard/ApplicationConfiguration.java
+++ api/src/main/java/ua/dropwizzard/ApplicationConfiguration.java
@@ -1,7 +1,60 @@
 package ua.dropwizzard;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import io.dropwizard.Configuration;
+import org.springframework.stereotype.Component;
 
+import javax.validation.constraints.NotNull;
+
+@Component
 public class ApplicationConfiguration extends Configuration {
 
+    @NotNull
+    private String datasourceUrl;
+    @NotNull
+    private String datasourceUserName;
+    @NotNull
+    private String datasourcePassword;
+    @NotNull
+    private String datasourceDriver;
+
+    @JsonProperty
+    public String getDatasourceUrl() {
+        return datasourceUrl;
+    }
+
+    @JsonProperty
+    public void setDatasourceUrl(String datasourceUrl) {
+        this.datasourceUrl = datasourceUrl;
+    }
+
+    @JsonProperty
+    public String getDatasourceUserName() {
+        return datasourceUserName;
+    }
+
+    @JsonProperty
+    public void setDatasourceUserName(String datasourceUserName) {
+        this.datasourceUserName = datasourceUserName;
+    }
+
+    @JsonProperty
+    public String getDatasourcePassword() {
+        return datasourcePassword;
+    }
+
+    @JsonProperty
+    public void setDatasourcePassword(String datasourcePassword) {
+        this.datasourcePassword = datasourcePassword;
+    }
+
+    @JsonProperty
+    public String getDatasourceDriver() {
+        return datasourceDriver;
+    }
+
+    @JsonProperty
+    public void setDatasourceDriver(String datasourceDriver) {
+        this.datasourceDriver = datasourceDriver;
+    }
 }
--- api/src/main/java/ua/spring/config/PersistenceContext.java
+++ api/src/main/java/ua/spring/config/PersistenceContext.java
@@ -4,32 +4,25 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.env.Environment;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import ua.dropwizzard.ApplicationConfiguration;
 
 import javax.sql.DataSource;
 
 @Configuration
 @ComponentScan("ua")
-@PropertySource("classpath:database.properties")
 public class PersistenceContext {
 
     @Autowired
-    Environment environment;
-
-    private final String URL = "url";
-    private final String USER = "dbuser";
-    private final String DRIVER = "driver";
-    private final String PASSWORD = "dbpassword";
+    ApplicationConfiguration applicationConfiguration;
 
     @Bean
     DataSource dataSource() {
         DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource();
-        driverManagerDataSource.setUrl((environment.getProperty(URL)));
-        driverManagerDataSource.setUsername(environment.getProperty(USER));
-        driverManagerDataSource.setPassword(environment.getProperty(PASSWORD));
-        driverManagerDataSource.setDriverClassName(environment.getProperty(DRIVER));
+        driverManagerDataSource.setUrl(applicationConfiguration.getDatasourceUrl());
+        driverManagerDataSource.setUsername(applicationConfiguration.getDatasourceUserName());
+        driverManagerDataSource.setPassword(applicationConfiguration.getDatasourcePassword());
+        driverManagerDataSource.setDriverClassName(applicationConfiguration.getDatasourceDriver());
         return driverManagerDataSource;
     }
 }
